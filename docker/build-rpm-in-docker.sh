f0134417a97ff6d82d8e4620ae5f5b89e860951a
Noilson Caio T. de Araújo <caiogore@gmail.com>
2017-01-05 16:56:32 -0300

    Build RPM  in Docker improvements
    List of files to be copy without directory - Maybe these files will stay in a separate folder. no more in root
    Variables standard names 
    Test for paths existence

--- docker/build-rpm-in-docker.sh
+++ docker/build-rpm-in-docker.sh
@@ -1,6 +1,48 @@
 #!/bin/sh
-cd $(dirname $0)
-mkdir -p build-dir-$1
-cp ../* build-dir-$1
-cp -r ../dist build-dir-$1/dist
-sudo docker run -i -t -v ${PWD}/build-dir-$1:/build/build-dir-$1 erlang-rpm-build-$1
+#
+#
+#
+centos_version="$1"
+build_dir="build-dir-$centos_version"
+packages=( "Makefile" "*.patch" "erlang.spec" "Erlang_ASL2_LICENSE.txt" )
+#
+#
+#
+if [ -z $centos_version ]
+then
+	echo "
+Ops: parameters error
+first: version, ex: 6 or 7
+-----------------------------------------
+Ex: ./build-rpm-in-docker.sh 7
+"
+	exit 1
+fi
+#
+#
+#
+if [ -e "$build_dir" ]
+then
+	rm -rf "$build_dir"
+	mkdir -p "$build_dir"
+else
+	mkdir "$build_dir"
+fi
+#
+#
+#
+for file in "${packages[@]}"
+do
+	cp ../$file "$build_dir"
+done
+#
+#
+#
+if [ -e ../dist ]
+then
+	cp -r ../dist "$build_dir"/dist
+fi
+#
+#
+#
+sudo docker run -i -t -v ${PWD}/"$build_dir":/build/"$build_dir" erlang-rpm-build-"$centos_version"
