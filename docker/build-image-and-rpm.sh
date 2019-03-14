985951abf765ac312dc3cf244ffdd1581566bf71
Michael Klishin <mklishin@pivotal.io>
2019-03-14 16:44:19 +0300

    Use sh instead of bash where possible

--- docker/build-image-and-rpm.sh
+++ docker/build-image-and-rpm.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 usage() {
   	echo "parameters error"
--- docker/build-packages.sh
+++ docker/build-packages.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 ./build-image-and-rpm.sh 7 --no-cache
 ./build-image-and-rpm.sh 6 --no-cache
