c79c6c865b5d89d4cf5d79f570799e9e723d2984
Michael Klishin <mklishin@pivotal.io>
2019-03-14 16:32:06 +0300

    docker/build-docker-image.sh: pass ShellCheck, support MacOS sed

--- docker/build-docker-image.sh
+++ docker/build-docker-image.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-#
-#	
-#
+
 docker_file="Dockerfile"
 docker_template="Dockerfile.template"
 centos_version="$1"
@@ -10,7 +8,7 @@ docker_dir="docker-centos-$centos_version"
 #
 #
 #
-if [ -z $centos_version ]
+if [ -z "$centos_version" ]
 then
 	echo "
 Ops: parameters error
@@ -22,34 +20,24 @@ Ex: ./build-docker-image.sh 7
 Ex: ./build-docker-image.sh 6 --no-cache
 EX: ./build-docker-image.sh 6
 "
-	exit 1
+exit 1
 fi
-#
-#
-#
+
 if [ -e "$docker_file" ]
 then
 	rm -f "$docker_file"
 fi
-#
-#
-#
+
 if [ -e "$docker_dir" ]
 then
 	rm -rf "$docker_dir"
-	mkdir "$docker_dir"
-else
-	mkdir "$docker_dir"
 fi
-#
-#
-#
+
+mkdir "$docker_dir"
+
 if [ -e "$docker_template" ]
 then
 	cp "$docker_template" "$docker_dir"/"$docker_file"
-	sed -i 's/{centosfrom}/'$centos_version'/g' "$docker_dir"/"$docker_file"
-	sudo docker build $docker_params -t="erlang-rpm-build-""$centos_version" "$docker_dir"
+	sed -i .bak "s/{centosfrom}/$centos_version/g" "$docker_dir"/"$docker_file"
+	sudo docker build "$docker_params" -t="erlang-rpm-build-""$centos_version" "$docker_dir"
 fi
-#
-#
-#
