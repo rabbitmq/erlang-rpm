f468291d0b7abd99e52623c080255f77820db3ce
Michael Klishin <mklishin@pivotal.io>
2019-10-15 21:22:57 +0300

    build-* scripts: attempts at improving Linux/GNU coreutils compat
    (cherry picked from commit 7d372dbd71a830973b9438a9a9ae3f3980e19ae8)
    
    Conflicts:
    	docker/build-docker-image.sh
    	docker/build-rpm-in-docker.sh

--- docker/build-docker-image.sh
+++ docker/build-docker-image.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#	
+#
 #
 docker_file="Dockerfile"
 docker_template="Dockerfile.template"
@@ -14,9 +14,10 @@ if [ -z $centos_version ]
 then
 	echo "
 Ops: parameters error
-first: version, ex: 6 or 7
-second: --no-cache [optional]
+first: version, ex: 8, 7, or 6
+second: docker build parameters such as --no-cache
 -----------------------------------------
+Ex: ./build-docker-image.sh 8 --no-cache
 Ex: ./build-docker-image.sh 7 --no-cache
 Ex: ./build-docker-image.sh 7
 Ex: ./build-docker-image.sh 6 --no-cache
@@ -41,15 +42,20 @@ then
 else
 	mkdir "$docker_dir"
 fi
-#
-#
-#
+
+mkdir "$docker_dir"
+
+echo "Will build an image for CentOS $centos_version using Docker file at $docker_dir/$docker_file"
+
 if [ -e "$docker_template" ]
 then
 	cp "$docker_template" "$docker_dir"/"$docker_file"
-	sed -i 's/{centosfrom}/'$centos_version'/g' "$docker_dir"/"$docker_file"
-	sudo docker build $docker_params -t="erlang-rpm-build-""$centos_version" "$docker_dir"
+	case $(uname -s) in
+		Linux)
+			sed --in-place=".bak" "s/{centosfrom}/$centos_version/g" "$docker_dir"/"$docker_file"
+			sudo docker build "$docker_params" -t="erlang-rpm-build-""$centos_version" "$docker_dir";;
+		*)
+			sed -i ".bak" "s/{centosfrom}/$centos_version/g" "$docker_dir"/"$docker_file"
+			docker build "$docker_params" -t="erlang-rpm-build-""$centos_version" "$docker_dir";;
+	esac
 fi
-#
-#
-#
