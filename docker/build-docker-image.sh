cfabab81c169a04e4b6a0a78cd69d83a84f65e24
Michael Klishin <klishinm@vmware.com>
2022-01-26 13:15:36 +0300

    Build system: initial support for CentOS Stream 9
     * Consume CentOS images from quay.io
     * Special case version 9 to point at the Stream 9 tag
    
    (cherry picked from commit cf797caedf979bad709ce33a9b675ce8a3b86b15)

--- docker/build-docker-image.sh
+++ docker/build-docker-image.sh
@@ -5,21 +5,28 @@ docker_template="Dockerfile.template"
 centos_version="$1"
 docker_params="$2"
 docker_dir="docker-centos-$centos_version"
-#
-#
-#
+
 if [ -z "$centos_version" ]
 then
 	echo "
 Ops: parameters error
-first: version, ex: 8, or 7
+first: version, ex: 9, 8, or 7
 second: docker build parameters such as --no-cache
 -----------------------------------------
+Ex: ./build-docker-image.sh 9 --no-cache
 Ex: ./build-docker-image.sh 8 --no-cache
 Ex: ./build-docker-image.sh 7 --no-cache
 "
 exit 1
 fi
+
+case $centos_version in
+	9)
+		centos_tag=stream9;;
+	*)
+		# 8 and 7
+		centos_tag="$centos_version"
+esac
 
 if [ -e "$docker_file" ]
 then
@@ -33,17 +40,17 @@ fi
 
 mkdir "$docker_dir"
 
-echo "Will build an image for CentOS $centos_version using Docker file at $docker_dir/$docker_file"
+echo "Will build an image for CentOS $centos_version (tag: $centos_tag) using Docker file at $docker_dir/$docker_file"
 
 if [ -e "$docker_template" ]
 then
 	cp "$docker_template" "$docker_dir"/"$docker_file"
 	case $(uname -s) in
 		Linux)
-			sed --in-place=".bak" "s/{centosfrom}/$centos_version/g" "$docker_dir"/"$docker_file"
+			sed --in-place=".bak" "s/{centosfrom}/$centos_tag/g" "$docker_dir"/"$docker_file"
 			sudo docker build "$docker_params" -t="erlang-rpm-build-""$centos_version" "$docker_dir";;
 		*)
-			sed -i ".bak" "s/{centosfrom}/$centos_version/g" "$docker_dir"/"$docker_file"
+			sed -i ".bak" "s/{centosfrom}/$centos_tag/g" "$docker_dir"/"$docker_file"
 			docker build "$docker_params" -t="erlang-rpm-build-""$centos_version" "$docker_dir";;
 	esac
 fi
