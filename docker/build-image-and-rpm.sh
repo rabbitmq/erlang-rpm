e57792695b90db3d9be2472d36079d26192a8fde
Michael Klishin <michael@clojurewerkz.org>
2025-05-09 15:46:44 -0400

    Local builds: switch to Rocky Linux #131

--- docker/build-image-and-rpm.sh
+++ docker/build-image-and-rpm.sh
@@ -3,11 +3,11 @@
 usage() {
 	echo "
 This script takes two arguments.
-first: distribution, one of stream9, stream8, al2023, fedora, rocky, alma, oracle
+first: distribution, one of rocky9, alma9, rocky8, al2023, fedora, oracle
 second: docker build parameters such as --no-cache
 -----------------------------------------
-Ex: ./build-image-and-rpm.sh stream9 --no-cache
-Ex: ./build-image-and-rpm.sh stream8 --no-cache
+Ex: ./build-image-and-rpm.sh rocky9 --no-cache
+Ex: ./build-image-and-rpm.sh rocky9 --no-cache
 Ex: ./build-image-and-rpm.sh fedora41 --no-cache
 "
   exit 1
