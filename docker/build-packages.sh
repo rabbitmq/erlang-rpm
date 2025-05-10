b61ff8a7531324958eb08c24059a2f1b464d2989
Michael Klishin <michaelklishin@icloud.com>
2025-05-10 00:09:12 -0400

    Local builds: build for/on Fedora 42 [ci skip]

--- docker/build-packages.sh
+++ docker/build-packages.sh
@@ -16,5 +16,4 @@ build_and_fetch_rpm_for "rocky9"
 build_and_fetch_rpm_for "rocky8"
 # These distributions cannot use CentOS Stream packages
 build_and_fetch_rpm_for "al2023"
-build_and_fetch_rpm_for "fc40"
-build_and_fetch_rpm_for "fc41"
+build_and_fetch_rpm_for "fc42"
